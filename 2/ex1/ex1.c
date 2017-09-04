806785514cd456228d703bd3bcea327cb0117027
Bili Xu <xubili93@gmail.com>
2017-09-04 13:25:39 +0800

    Add lab 2 skeleton code

---
+++ 2/ex1/ex1.c
@@ -0,0 +1,29 @@
+/*************************************
+* Lab 2 Exercise 1
+* Name:
+* Student No:
+* Lab Group:
+*************************************
+Warning: Make sure your code works on
+lab machine (Linux on x86)
+*************************************/
+
+#include <stdio.h>
+#include <sys/types.h>   
+#include <sys/stat.h>
+#include <unistd.h>     //for fork()
+#include <sys/wait.h>   //for wait()
+
+int main()
+{
+    int nChild;
+
+    //Read the number of child
+    scanf("%d", &nChild);
+
+    //Spawn child processes
+
+    //Wait on child processes in order
+     
+    return 0;
+}
