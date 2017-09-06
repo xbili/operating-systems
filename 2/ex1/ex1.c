abce8e4f121d23ac751af589690779e1450e5091
Bili Xu <xubili93@gmail.com>
2017-09-06 21:06:06 +0800

    Lab2 exercise 1 implementation

--- 2/ex1/ex1.c
+++ 2/ex1/ex1.c
@@ -1,15 +1,15 @@
 /*************************************
 * Lab 2 Exercise 1
-* Name:
-* Student No:
-* Lab Group:
+* Name: Xu Bili
+* Student No: A0124368A
+* Lab Group: 4
 *************************************
 Warning: Make sure your code works on
 lab machine (Linux on x86)
 *************************************/
 
 #include <stdio.h>
-#include <sys/types.h>   
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>     //for fork()
 #include <sys/wait.h>   //for wait()
@@ -21,9 +21,30 @@ int main()
     //Read the number of child
     scanf("%d", &nChild);
 
-    //Spawn child processes
+    // Make sure that N is between 0 and 9
+    if (nChild < 1 || nChild > 9) return 1;
 
-    //Wait on child processes in order
-     
+    pid_t pids[nChild];
+
+    int childPid, pid;
+    for (int i = 0; i < nChild; i++) {
+        childPid = fork();
+        pid = getpid();
+        if (childPid != 0) { // Parent
+            pids[i] = childPid;
+        } else { // Child
+            printf("Child %d[%d]: Hello!\n", i, pid);
+            return 0; // Exit
+        }
+    }
+
+    // Wait on child processes in order
+    for (int i = 0; i < nChild; i++) {
+        childPid = waitpid(pids[i], NULL, 0);
+        printf("Parent: Child %d[%d] done.\n", i, childPid);
+    }
+
+    printf("Parent: Exiting.\n");
+
     return 0;
 }
