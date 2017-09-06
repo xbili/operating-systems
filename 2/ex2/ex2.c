2ffadc97d4dab9d3f6efc7fedd20ca86fd0a80e3
Bili Xu <xubili93@gmail.com>
2017-09-06 21:46:45 +0800

    Initial implementation for lab2 ex2

--- 2/ex2/ex2.c
+++ 2/ex2/ex2.c
@@ -1,8 +1,8 @@
 /*************************************
 * Lab 2 Exercise 2
-* Name:
-* Student No:
-* Lab Group:
+* Name: Xu Bili
+* Student No: A0124368A
+* Lab Group: 4
 *************************************
 Warning: Make sure your code works on
 lab machine (Linux on x86)
@@ -11,7 +11,7 @@ lab machine (Linux on x86)
 #include <stdio.h>
 #include <string.h>     //For string comparison, string copy
 #include <fcntl.h>      //For stat()
-#include <sys/types.h>   
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>     //for fork()
 #include <sys/wait.h>   //for wait()
@@ -19,21 +19,48 @@ lab machine (Linux on x86)
 int main()
 {
     char path[20];
+    char last[20];
 
-    //read user input
+    // Read user input
     printf("YWIMC > ");
-    scanf("%s", path );
+    scanf("%s", path);
 
-    while (strcmp( path, "quit") != 0){ 
+    struct stat fileStat;
+    pid_t childPid;
+    while (strcmp(path, "quit") != 0) {
         // Check whether file exist
         // In real interpreter, a lot more checks are needed
         // E.g. check for file type, execution right etc
 
+        // TODO: Make sure that if there is no last command available, skip
+        // the while-loop
+        if (last[0] != '\0' && strcmp(path, "last") == 0) {
+            strcpy(path, last);
+        } else if (strcmp(path, "last") == 0) {
+            continue;
+        }
+
+        if (stat(path, &fileStat) == -1) {
+            printf("%s not found\n", path);
+        } else if (S_ISREG(fileStat.st_mode)) {
+            // Make sure that found file is a executable process
+
+            childPid = fork();
+            if (childPid != 0) { // Parent
+                waitpid(childPid, NULL, 0);
+            } else { // Child
+                execl(path, path);
+            }
+
+            strcpy(last, path);
+        } else {
+            printf("%s is not an executable.\n", path);
+        }
 
         printf("YWIMC > ");
         scanf("%s", path);
     }
-    
+
     printf("Goodbye!\n");
     return 0;
 }
