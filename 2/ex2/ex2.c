6abbe257724c4d4022b652c4ab7fa59ba5c95f5a
Bili Xu <xubili93@gmail.com>
2017-09-07 14:20:45 +0800

    Refactor lab2 ex2 methods and include documentation

--- 2/ex2/ex2.c
+++ 2/ex2/ex2.c
@@ -16,15 +16,59 @@ lab machine (Linux on x86)
 #include <unistd.h>     //for fork()
 #include <sys/wait.h>   //for wait()
 
+
+/*************
+ * CONSTANTS *
+ *************/
+
 #define NON_EXECUTABLE 1
 #define NOT_FOUND 2
 
+
+/*************************
+ * FUNCTION DECLARATIONS *
+ *************************/
+
+/**
+ * Returns the valid file descriptor for the input path.
+ *
+ * Returns:
+ * - NON_EXECUTABLE: if the file is not an executable
+ * - NOT_FOUND: if the file directory does not exist
+ * - 0: if the file exists and can be run
+ */
 int fileCheck(char *path);
+
+/**
+ * Reads a line of input from the user.
+ */
 void readInput(char *path);
+
+/**
+ * Handles an invalid command
+ */
 void invalidCommand(char *path);
-void updateCommand(char *path, char *prev);
-void savePrevCommand(char *path, char *prev);
+
+/**
+ * Parses the command input by the user, transforming the command if
+ * necessary.
+ */
+void parseCommand(char *path, char *prev);
+
+/**
+ * Saves the previous command for use if `last` is invoked.
+ */
+void rememberCommand(char *path, char *prev);
+
+/**
+ * Create a new child process that runs the program located at `path`.
+ */
 void spawn(char *path);
+
+
+/********
+ * MAIN *
+ ********/
 
 int main()
 {
@@ -37,6 +81,7 @@ int main()
         // In real interpreter, a lot more checks are needed
         // E.g. check for file type, execution right etc
 
+        // TODO: Change this to make use of function pointers
         int fd = fileCheck(path);
         switch (fd) {
             case NON_EXECUTABLE:
@@ -49,20 +94,19 @@ int main()
                 spawn(path);
         }
 
-        savePrevCommand(path, last);
+        rememberCommand(path, last);
         readInput(path);
-        updateCommand(path, last);
+        parseCommand(path, last);
     }
 
     printf("Goodbye!\n");
     return 0;
 }
 
-void readInput(char *path) {
-    printf("YWIMC > ");
-    scanf("%s", path);
-}
 
+/***************************
+ * FUNCTION IMPLEMENTATION *
+ ***************************/
 
 /**
  * Returns the valid file descriptor for the input path.
@@ -87,6 +131,19 @@ int fileCheck(char *path)
 }
 
 
+/**
+ * Reads a line of input from the user.
+ */
+void readInput(char *path)
+{
+    printf("YWIMC > ");
+    scanf("%s", path);
+}
+
+
+/**
+ * Handles an invalid command
+ */
 void invalidCommand(char *path)
 {
     if (strcmp(path, "last") == 0) {
@@ -97,6 +154,36 @@ void invalidCommand(char *path)
 }
 
 
+/**
+ * Checks for the `last` command, we do not update the command unless `last`
+ * is invoked.
+ *
+ * If `last` is invoked, we will update our current command to that of the
+ * previous command that we input into the interpreter.
+ */
+void parseCommand(char *path, char *prev)
+{
+    if (strcmp(path, "last") == 0 && strcmp(prev, "") != 0) {
+        strcpy(path, prev);
+    }
+}
+
+
+/**
+ * Saves user's previous command only if path is not `last`. This is to prevent
+ * an infinite loop.
+ */
+void rememberCommand(char *path, char *prev)
+{
+    if (strcmp(path, "last") != 0) {
+        strcpy(prev, path);
+    }
+}
+
+
+/**
+ * Spawns a new child process that runs the program located at `path`.
+ */
 void spawn(char *path)
 {
     pid_t childPid;
@@ -106,25 +193,6 @@ void spawn(char *path)
         waitpid(childPid, NULL, 0);
     } else { // Child
         execl(path, path, NULL);
-    }
-}
-
-
-// Save previous command only if path is not `last`.
-// This is to prevent an infinite loop.
-void savePrevCommand(char *path, char *prev)
-{
-    if (strcmp(path, "last") != 0) {
-        strcpy(prev, path);
-    }
-}
-
-
-// Checks for the `last` command, and update the path accordingly
-void updateCommand(char *path, char *prev)
-{
-    if (strcmp(path, "last") == 0 && strcmp(prev, "") != 0) {
-        strcpy(path, prev);
     }
 }
 
