7087d22de4c29e4a5a77b24f02180cc513509391
Bili Xu <xubili93@gmail.com>
2017-10-14 00:14:34 +0800

    Lab 4 skeleton

---
+++ 4/ex1/mmalloc.h
@@ -0,0 +1,30 @@
+#ifndef __MMALLOC_H_	//standard protection against multiple includes
+#define __MMALLOC_H_
+
+#include <unistd.h>	//for brk, sbrk
+
+#define FREE 0
+#define OCCUPIED 1
+
+typedef struct bm {
+	int	size;
+	struct bm *nextPart;
+	int status;
+} partMetaInfo;
+
+
+typedef struct {
+	partMetaInfo* base;
+	int totalSize;
+	int partMetaSize;
+} heapMetaInfo;
+
+
+int setupHeap(int);
+void printHeapMetaInfo();
+void printHeapStatistic();
+
+void* malloc(int);
+void free(void*);
+
+#endif
