4ad5f323366edb5c48da163004ac8a9162a1dd4b
Bili Xu <xubili93@gmail.com>
2017-11-08 10:21:12 +0800

    Implement lab 5 exercise 2

--- 5/ex2/ex2.c
+++ 5/ex2/ex2.c
@@ -1,8 +1,8 @@
 /*************************************
 * Lab 5 Exercise 2
-* Name:
-* Student No:
-* Lab Group:
+* Name: Xu Bili
+* Student No: A0124368A
+* Lab Group: 4
 *************************************/
 
 #include <stdio.h>
@@ -12,9 +12,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define COMMAND_READ 1
+#define COMMAND_FORWARD 2
+#define COMMAND_BACKWARD 3
+
+void printOffset(off_t offset);
+int readAndPrintInt(int fdIn, int nItems, int nBytes);
+int readAndPrintChar(int fdIn, int nItems, int nBytes);
+
 int main()
 {
-    int fdIn, option, nItems, nBytes;
+    int fdIn, option, nItems, nBytes, fileSize;
     char fileName[40];
 
     //Checking 32-bit vs 64-bit. Do not remove!
@@ -33,18 +41,93 @@ int main()
     scanf("%s", fileName);
 
     fdIn = open(  fileName, O_RDONLY );
+    if (fdIn == -1) {
+        printf("Cannot Open\n");
+        return 1;
+    }
 
-    //TODO: Copy from ex1, Check for valid file
-
-    //TODO: Copy from ex1. Calculate the file size
+    fileSize = lseek(fdIn, 0, SEEK_END); // Read to the end of the file
+    printf("Size = %i bytes\n", fileSize);
+    lseek(fdIn, 0, SEEK_SET); // Reset the FD to start of the file again
 
     //Read and perform the specified options
-    while( scanf("%i %i %i", &option, &nItems, &nBytes) == 3){
-        //TODO: Handle the options here
+    off_t offset = 0;
+    while( scanf("%i %i %i", &option, &nItems, &nBytes) == 3) {
+        switch (option) {
+            case COMMAND_READ:
+                if (nBytes == 4) {
+                    offset += readAndPrintInt(fdIn, nItems, nBytes);
+                } else if (nBytes == 1) {
+                    offset += readAndPrintChar(fdIn, nItems, nBytes);
+                } else {
+                    printf("failed");
+                    break;
+                }
 
+                break;
+            case COMMAND_FORWARD:
+                if (offset + nItems * nBytes > fileSize) {
+                    printf("not allowed\n");
+                    break;
+                }
+
+                offset = lseek(fdIn, nItems * nBytes, SEEK_CUR);
+                printOffset(offset);
+                break;
+            case COMMAND_BACKWARD:
+                offset = lseek(fdIn, - nItems * nBytes, SEEK_CUR);
+                printOffset(offset);
+                break;
+            default:
+                printf("Invalid command.\n");
+                return 1;
+        }
     }
-    
+
     close( fdIn );  //good practice
 
     return 0;
+}
+
+// HELPER
+// Prints out offset if the current offset value is valid.
+void printOffset(off_t offset)
+{
+    if (offset == -1) {
+        printf("not allowed\n");
+    } else {
+        printf("%d\n", (int) offset);
+    }
+}
+
+// HELPER
+// Read and print out integers from file.
+//
+// Returns the number of bytes read from file.
+int readAndPrintInt(int fdIn, int nItems, int nBytes)
+{
+    int buffer[nItems];
+    int bytesRead = read(fdIn, buffer, nItems * nBytes);
+    for (int i = 0; i < bytesRead / nBytes; i++) {
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
+
+    return bytesRead;
+}
+
+// HELPER
+// Read and print out characters from file.
+//
+// Returns the number of bytes read from file.
+int readAndPrintChar(int fdIn, int nItems, int nBytes)
+{
+    char buffer[nItems];
+    int bytesRead = read(fdIn, buffer, nItems * nBytes);
+    for (int i = 0; i < bytesRead / nBytes; i++) {
+        printf("%c ", buffer[i]);
+    }
+    printf("\n");
+
+    return bytesRead;
 }
